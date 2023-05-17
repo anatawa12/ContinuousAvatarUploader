b5ad485aae7fe6a16e1222e0faac4bdf4c63c30c
anatawa12 <anatawa12@icloud.com>
2023-05-17 17:14:09 +0900

    feat: initial implementation for version name in description

--- Editor/ContinuousAvatarUploader.cs
+++ Editor/ContinuousAvatarUploader.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Diagnostics;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEditor.SceneManagement;
@@ -9,6 +12,7 @@ using UnityEngine.UI;
 using VRC.Core;
 using VRC.SDK3.Avatars.Components;
 using VRC.SDKBase.Editor;
+using Debug = UnityEngine.Debug;
 using Task = System.Threading.Tasks.Task;
 
 namespace Anatawa12.ContinuousAvatarUploader.Editor
@@ -22,7 +26,7 @@ namespace Anatawa12.ContinuousAvatarUploader.Editor
     {
         [SerializeField] State state;
         [SerializeField] int processingIndex = -1;
-        [SerializeField] AvatarDescriptor[] avatarDescriptors;
+        [SerializeField] AvatarDescriptor[] avatarDescriptors = Array.Empty<AvatarDescriptor>();
         [SerializeField] AvatarDescriptor uploadingAvatar;
 
         private SerializedObject _serialized;
@@ -129,8 +133,21 @@ namespace Anatawa12.ContinuousAvatarUploader.Editor
             while (nameField.text == "")
                 await Task.Delay(2500);
 
-            // TODO: update description
-            descriptionField.text = descriptionField.text + " Test Upload using ContinuousAvatarUploader";
+            var platformInfo = EditorUserBuildSettings.selectedBuildTargetGroup == BuildTargetGroup.Standalone
+                ? uploadingAvatar.windows
+                : uploadingAvatar.quest;
+
+            if (platformInfo.versionNameEnabled)
+            {
+                long versionName;
+                (descriptionField.text, versionName) =
+                    UpdateVersionName(descriptionField.text, platformInfo.versionNamePrefix);
+                if (platformInfo.gitEnabled)
+                {
+                    var tagName = platformInfo.tagPrefix + versionName + platformInfo.tagSuffix;
+                    AddGitTag(tagName);
+                }
+            }
 
             warrantToggle.isOn = true;
 
@@ -140,6 +157,88 @@ namespace Anatawa12.ContinuousAvatarUploader.Editor
             };
 
             uploadButton.OnPointerClick(new PointerEventData(EventSystem.current));
+        }
+
+        private (string, long) UpdateVersionName(string description, string versionPrefix)
+        {
+            var escapedPrefix = Regex.Escape(versionPrefix);
+            var regex = new Regex($@"[(（]{escapedPrefix}(\d+)[)）]");
+
+            var match = regex.Match(description);
+            if (match.Success)
+            {
+                var capture = match.Groups[1].Captures[0];
+                if (long.TryParse(capture.Value, out var versionName))
+                {
+                    versionName += 1;
+                    var prefix = description.Substring(0, capture.Index);
+                    var suffix = description.Substring(capture.Index + capture.Length);
+                    return (prefix + versionName + suffix, versionName);
+                }
+            }
+
+            return (description + $" ({versionPrefix}1)", 1);
+        }
+
+        private void AddGitTag(string tagName)
+        {
+            try
+            {
+                // ArgumentList is not implemented in Unity 2019.
+                using (var p = Process.Start("git", $"tag -- {EscapeForProcessArgument(tagName)}"))
+                {
+                    System.Diagnostics.Debug.Assert(p != null, nameof(p) + " != null");
+                    p.WaitForExit();
+                    System.Diagnostics.Debug.Assert(p.ExitCode == 0, 
+                        $"git command exit with non zer value: {p.ExitCode}");
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(new Exception($"Tagging version {tagName} for {uploadingAvatar.name}", e));
+            }
+
+            string EscapeForProcessArgument(string argument)
+            {
+                if (!argument.Any(c => char.IsWhiteSpace(c) || c == '"'))
+                    return argument;
+                var builder = new StringBuilder();
+                builder.Append('"');
+
+                var idx = 0;
+                while (idx < argument.Length)
+                {
+                    var c = argument[idx++];
+                    switch (c)
+                    {
+                        case '\\':
+                        {
+                            int numBackSlash = 1;
+                            while (idx < argument.Length && argument[idx] == '\\')
+                            {
+                                idx++;
+                                numBackSlash++;
+                            }
+
+                            if (idx == argument.Length || argument[idx] == '"')
+                                builder.Append('\\', numBackSlash * 2);
+                            else
+                                builder.Append('\\', numBackSlash);
+                            break;
+                        }
+                        case '"':
+                            builder.Append('\\').Append('"');
+                            break;
+                        default:
+                            builder.Append(c);
+                            break;
+                    }
+                } 
+
+                builder.Append('"');
+
+                return builder.ToString();
+            }
         }
 
         private async Task<bool> Upload(AvatarDescriptor avatar)
