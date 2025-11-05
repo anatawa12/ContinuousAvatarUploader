eef515d6dc351839e8fd1d3f58db81b5578c3d68
Misaka-L <lipww1234@foxmail.com>
2025-11-05 20:25:22 +0800

    refactor: remove ContinuousAvatarUploaderApi

--- Editor/ContinuousAvatarUploaderApi.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Anatawa12.ContinuousAvatarUploader.Editor
-{
-    public static class ContinuousAvatarUploaderApi
-    {
-        public static bool IsUploadInProgress => UploadOrchestrator.IsUploadInProgress();
-        public static void CancelUpload() => UploadOrchestrator.CancelUpload();
-    }
-}
